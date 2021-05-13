71cbb14e106d5b8409b4b4cd254d045f7bec056f
Marc Marina Miravitlles <marc.marina.miravitlles@gmail.com>
2021-05-13 22:11:20 +0100

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+
+async function startApolloServer() {
+  const typeDefs = gql`
+    type Query {
+      hello: String
+    }
+  `;
+
+  const resolvers = {
+    Query: {
+      hello: () => 'Hello world!',
+    },
+  };
+
+  const server = new ApolloServer({ typeDefs, resolvers });
+  await server.start();
+
+  const app = express();
+  server.applyMiddleware({ app });
+
+  await new Promise(resolve => app.listen({ port: 4000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+  return { server, app };
+}
